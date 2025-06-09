20ba0c4ffc2a03e2d9617825c7b88907f08b4e1b
Daniel Keerie <daniel@keerie.co.uk>
2025-06-09 22:54:51 +0100

    Finished solver, created a print function and applied it to both plane polar coordinates and spherical polar coordinates

--- Christoffel Symbols.py
+++ Christoffel Symbols.py
@@ -3,22 +3,45 @@ from sympy import *
 init_printing(use_unicode=True)
 from sympy import MutableDenseNDimArray
 
-
-
-
-# M = sp.Matrix([[4,2], [1, 0]])
-# print(M.shape)
-# print(M[0,1])
-# print(M)
-
 r, theta = sp.symbols('r theta')
+coords = [r, theta]
 
-g = sp.Matrix([[1, 0], [0, r**2]])
+g_polar = sp.Matrix([[1, 0], [0, r**2]])
 
-def christoffel_symbols(g):
-    n = g.shape[0]
+
+def christoffel_symbols(g, coords):
+    n = len(coords)
+    g_inv = g.inv()
     Gamma = MutableDenseNDimArray.zeros(n, n, n)
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                for l in range(n):
+                    Gamma[i, j, k] += 0.5*g_inv[i, l]*(sp.diff(g[l,j], coords[k]) + sp.diff(g[l,k], coords[l]) - sp.diff(g[j,k], coords[l])) #+= is required as we're summing over different l contributions
+                    Gamma[i,j,k] = sp.simplify(sp.trigsimp(Gamma[i,j,k]))
+    return Gamma
 
-n = g.shape[0]
-Gamma = MutableDenseNDimArray.zeros(n, n, n)
-print(Gamma)
+Gamma = christoffel_symbols(g_polar, coords)
+print(Gamma[0, 1, 1])           
+
+def display_christoffel(Gamma, coords):
+    n = len(coords)
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                if Gamma[i,j,k] != 0:
+                    print(f"Γ^{i}_[{j}{k}] = {Gamma[i,j,k]}")
+    #tried doing coords[i, j, k] but it looks better with just 0s and 1s in the printout
+    print("Rest are zero.")
+
+display_christoffel(Gamma, coords)
+
+# Now do same for spherical coordinates
+r, theta, phi = sp.symbols('r theta phi')
+coords_spherical = [r, theta, phi]
+
+g_spherical = sp.Matrix([[1, 0, 0], [0, r**2, 0], [0, 0, r**2 * sp.sin(theta)**2]])
+
+Gamma_spherical = christoffel_symbols(g_spherical, coords_spherical)
+display_christoffel(Gamma_spherical, coords_spherical)
+
