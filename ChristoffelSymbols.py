91cf6c9e0755e9e6e3fc0748e0ee1a666026e265
Daniel Keerie <daniel@keerie.co.uk>
2025-06-10 22:01:20 +0100

    Ran into issues trying to import functions so ran a test on that, issue was with the file name for the Christoffel calculator. Also made a Riemann tensor calculator but this was done roughly so needs checked. Also want to streamline all the functions so that only one function needs called to calculate the Riemann tensor in a single file

--- ChristoffelSymbols.py
+++ ChristoffelSymbols.py
@@ -1,13 +1,12 @@
 import sympy as sp
 from sympy import *
 init_printing(use_unicode=True)
-from sympy import MutableDenseNDimArray
+# from sympy import MutableDenseNDimArray
 
 r, theta = sp.symbols('r theta')
 coords = [r, theta]
 
 g_polar = sp.Matrix([[1, 0], [0, r**2]])
-
 
 def christoffel_symbols(g, coords):
     n = len(coords)
@@ -26,6 +25,7 @@ print(Gamma[0, 1, 1])
 
 def display_christoffel(Gamma, coords):
     n = len(coords)
+    print(n)
     for i in range(n):
         for j in range(n):
             for k in range(n):
@@ -33,10 +33,11 @@ def display_christoffel(Gamma, coords):
                     print(f"Γ^{i}_[{j}{k}] = {Gamma[i,j,k]}")
     #tried doing coords[i, j, k] but it looks better with just 0s and 1s in the printout
     print("Rest are zero.")
+    print()
 
 display_christoffel(Gamma, coords)
 
-# Now do same for spherical coordinates
+#Now do same for spherical coordinates
 r, theta, phi = sp.symbols('r theta phi')
 coords_spherical = [r, theta, phi]
 
@@ -45,3 +46,14 @@ g_spherical = sp.Matrix([[1, 0, 0], [0, r**2, 0], [0, 0, r**2 * sp.sin(theta)**2
 Gamma_spherical = christoffel_symbols(g_spherical, coords_spherical)
 display_christoffel(Gamma_spherical, coords_spherical)
 
+#Schwarzschild Metric
+
+t, r, theta, phi = sp.symbols('t r theta phi')
+M = sp.symbols('M')
+# G=c=1
+coords_schwarz = [t, r, theta, phi]
+#using [-, +, +, +] signature
+g_schwarz = sp.Matrix([[-(1-2*M/r), 0, 0, 0], [0, (1-2*M/r)**(-1), 0, 0], [0, 0, r**2, 0], [0, 0, 0, (r**2)*(sp.sin(theta))**2]])
+
+Gamma_schwarz = christoffel_symbols(g_schwarz, coords_schwarz)
+display_christoffel(Gamma_schwarz, coords_schwarz)
---
+++ RiemannT.py
@@ -0,0 +1,58 @@
+from test import function
+from ChristoffelSymbols import *
+# IMPORTING FUNCTIONS NEEDS WORK!!
+
+import sympy as sp
+from sympy import *
+init_printing(use_unicode=True)
+# from sympy import MutableDenseNDimArray
+
+#Redefine Christoffel Function
+def christoffel_symbols(g, coords):
+    n = len(coords)
+    g_inv = g.inv()
+    Gamma = MutableDenseNDimArray.zeros(n, n, n)
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                for l in range(n):
+                    term1 = sp.diff(g[l,j], coords[k])
+                    term2 = sp.diff(g[l,k], coords[j])
+                    term3 = sp.diff(g[j,k], coords[l])
+                    Gamma[i,j,k] += 0.5*g_inv[i, l]*(term1+term2-term3)
+                    Gamma[i,j,k] = sp.simplify(sp.trigsimp(Gamma[i,j,k]))
+    return Gamma
+
+#printer function
+def display_christoffel(Gamma, coords):
+    n = len(coords)
+    print(n)
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                if Gamma[i,j,k] != 0:
+                    print(f"Γ^{i}_[{j}{k}] = {Gamma[i,j,k]}")
+    #tried doing coords[i, j, k] but it looks better with just 0s and 1s in the printout
+    print("Rest are zero.")
+    print()
+
+#Riemann tensor
+def Riemann_Curvature(g, coords, Gamma):
+    n = len(coords)
+    R = Gamma = MutableDenseNDimArray.zeros(n, n, n, n)
+    for i in range(n):
+        for j in range(n):
+            for k in range(n):
+                for l in range(n):
+                    for m in range(n):
+                        term1 = sp.diff(Gamma[i, j, l], coords[k])
+                        term2 = sp.diff(Gamma[i, j, k], coords[l])
+                        term3 = Gamma[i, m, k]*Gamma[m, j, l]
+                        term4 = Gamma[i, m, l]*Gamma[m, j, k]
+                        R[i, j, k, l] += term1 - term2 + term3 - term4
+    return R
+
+#Ricci tensor
+# def Ricci_Tensor(g, R):
+
+
