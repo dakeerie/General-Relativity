53f37487c97c5b3c98ba7d3c672dd28ec2fab41d
Daniel Keerie <daniel@keerie.co.uk>
2025-06-15 20:22:49 +0100

    .

--- LorentzBoost.py
+++ LorentzBoost.py
@@ -7,25 +7,20 @@ def lorentz_factor(v):
     gamma = (1-v**2)**(-1/2)
     return gamma
 
-
-array = np.array([1,2,3,4])
-a,b,c,d = array[0:4]
-print(c)
-
 def lorentz_boost(v, event):
     #v in natural units
     #lorentz boost of velocity v in the x direction
-    # t, x, y, z = event[0:3]
+    t, x, y, z = event[0:4]
     barred_coordinates = np.zeros(4)
     gamma = lorentz_factor(v)
 
-    # t,x,y,z = event[0:3]
+    barred_coordinates[0] = gamma*t - gamma*v*x
+    barred_coordinates[1] = -gamma*v*t + gamma*x
+    barred_coordinates[2] = y #y and z coordinates the same for boost in x direction alone
+    barred_coordinates[3] = z
 
-    barred_coordinates[0] = gamma*event[0] - gamma*v*event[1]
-    barred_coordinates[1] = -gamma*v*event[0] + gamma*event[1]
-    barred_coordinates[2] = event[2] #y and z coordinates the same for boost in x direction alone
-    barred_coordinates[3] = event[3]
-    #CHECK FORMULAE AND T, X, Y, Z LINE!!
+    t_prime, x_prime, y_prime, z_prime = barred_coordinates[0:4]
+    
     return barred_coordinates
 
 #Add boost in any direction. Velocity 3-vector?
