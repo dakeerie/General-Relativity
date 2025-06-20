dd06fc80bd225022c510762702688c38da862d43
Daniel Keerie <daniel@keerie.co.uk>
2025-06-20 23:31:15 +0100

    made a second plotter with the capacity to plot the t' and x' axes for a given velocity using the lorentz boost equation

--- Week1/MinkowskiDiagram.py
+++ Week1/MinkowskiDiagram.py
@@ -1,6 +1,176 @@
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 from InvariantInterval import interval
 from LorentzBoost import lorentz_boost, lorentz_factor
+
+mpl.rcParams.update({
+    "text.usetex": False,
+    "font.family": "sans-serif",
+    "font.sans-serif": ["DejaVu Sans"],
+    "axes.labelsize": 14,
+    "font.size": 14,
+    "xtick.labelsize": 10,
+    "ytick.labelsize": 10,
+})
+
+import random
+ 
+def random_colour_generator():
+    r = 0
+    g = random.randint(0, 125)/255
+    b = random.randint(0, 255)/255
+    return (r, g, b)
+
+def light_cone_plot(events, size, shade_common_future = False, shade_common_past = False):
+    fig, ax = plt.subplots(figsize = [6,6])
+
+    ax.set_xlim(-size, size)
+    ax.set_ylim(-size, size)
+
+    ax.spines['left'].set_position('center')
+    ax.spines['bottom'].set_position('center')
+
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+
+    ax.set_xlabel('x', loc='right', fontname = 'DejaVu Sans')
+    ax.set_ylabel('t', rotation=0, loc='top', fontname = 'DejaVu Sans')
+
+    c = np.linspace(-size, size, 100)
+    # origin light cone
+    # ax.plot(c, c, linestyle = '--', alpha = 0.5, color = 'blue')
+    # ax.plot(c, -c, linestyle = '--', alpha = 0.5, color = 'blue')
+
+    for event in events:
+        x_coord = event[1]
+        t_coord = event[0]
+
+        colour = random_colour_generator()
+        ax.scatter(x_coord, t_coord, marker = 'o', s = 10, color = colour, zorder = 11, label = 'Event ' + str(events.index(event) + 1))
+        ax.plot(c, t_coord + c - x_coord, linestyle = '--', color = colour, zorder = 10, label = 'Light cone of event ' + str(events.index(event) + 1))
+        ax.plot(c, t_coord - c + x_coord, linestyle = '--', color = colour, zorder = 10)
+
+    # if shade_common_future and len(events) > 1:
+    #     t_min = max(t for t, _ in events)
+    #     t_vals = np.linspace(t_min, size, 500)
+
+    #     left = np.full_like(t_vals, -np.inf)
+    #     right = np.full_like(t_vals, np.inf)
+
+    #     for t_i, x_i in events:
+    #     # Only defined for t_vals ≥ t_i
+    #         mask = t_vals >= t_i
+    #         left_event = np.full_like(t_vals, -np.inf)
+    #         right_event = np.full_like(t_vals, np.inf)
+
+    #         left_event[mask] = x_i - (t_vals[mask] - t_i)
+    #         right_event[mask] = x_i + (t_vals[mask] - t_i)
+
+    #         left = np.maximum(left, left_event)
+    #         right = np.minimum(right, right_event)
+
+    #     valid = left < right
+    #     ax.fill_between(t_vals[valid], left[valid], right[valid], color='orange', alpha=0.3)
+
+
+    # if shade_common_past and len(events) > 1:
+    #     t_max = min(t for t, _ in events)
+    #     t_vals = np.linspace(-size, t_max, 500)
+
+    #     left = np.full_like(t_vals, -np.inf)
+    #     right = np.full_like(t_vals, np.inf)
+
+    #     for t_i, x_i in events:
+    #     # Only defined for t_vals ≤ t_i
+    #         mask = t_vals <= t_i
+    #         left_event = np.full_like(t_vals, -np.inf)
+    #         right_event = np.full_like(t_vals, np.inf)
+
+    #         left_event[mask] = x_i - (t_i - t_vals[mask])
+    #         right_event[mask] = x_i + (t_i - t_vals[mask])
+
+    #         left = np.maximum(left, left_event)
+    #         right = np.minimum(right, right_event)
+
+    #     valid = left < right
+    #     ax.fill_between(t_vals[valid], left[valid], right[valid], color='purple', alpha=0.3)
+
+    grid_ticks = range(-size, size + 1, 1)
+    ax.set_xticks(grid_ticks, minor=False)
+    ax.set_yticks(grid_ticks, minor=False)
+    ax.tick_params(left=False, bottom=False, labelleft=False, labelbottom=False)
+    ax.grid(True, which='both', axis='both', linestyle='--', alpha=0.3)
+    ax.text(-0.1, -0.1, '0', fontsize=12, ha='right', va='top', fontname='DejaVu Sans')
+    ax.legend(loc = 'best', fontsize = 8)
+    ax.figure.canvas.draw_idle()
+
+    plt.show()
+
+test = [[0, 1], [1, 4]]
+# light_cone_plot(test, 10, True, True)
+
+
+def minkowski_diagram(events, vs, size, hyperbolae = False):
+    fig, ax = plt.subplots(figsize = [6,6])
+
+    ax.set_xlim(-size, size)
+    ax.set_ylim(-size, size)
+    ax.spines['left'].set_position('center')
+    ax.spines['bottom'].set_position('center')
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.yaxis.set_ticks_position('left')
+    ax.set_xlabel('x', loc='right', fontname = 'DejaVu Sans')
+    ax.set_ylabel('t', rotation=0, loc='top', fontname = 'DejaVu Sans')
+
+    c = np.linspace(-size, size, 100)
+
+    for n in range(len(events)):
+        event = events[n]
+        x_coord = event[1]
+        t_coord = event[0]
+        colour = random_colour_generator()
+        ax.scatter(x_coord, t_coord, marker = 'o', s = 10, color = colour, zorder = 11, label = 'Event ' + str(events.index(event) + 1))
+        # ax.plot(c, t_coord + c - x_coord, linestyle = '--', color = colour, zorder = 10, label = 'Light cone of event ' + str(events.index(event) + 1))
+        # ax.plot(c, t_coord - c + x_coord, linestyle = '--', color = colour, zorder = 10)
+    
+    for l in range(len(vs)):
+        v = vs[l]
+        if np.abs(v) <= 1:
+            plt.plot(c, c/v, linestyle = ':', color = 'k') # x' axis has t' = 0
+            plt.plot(c, v*c, linestyle = ':', color = 'k') # t' axis has x' = 0
+        else:
+            raise ValueError(f"velocity v={v} must satisfy |v| <= 1.")
+        
+    if hyperbolae == True:
+        plt.plot(c, np.sqrt(c**2-1), color = 'k', linestyle = '--', label = 'Calibration hyperbolae')
+        plt.plot(c, -np.sqrt(c**2-1), color = 'k', linestyle = '--')
+        plt.plot(c, np.sqrt(c**2+1), color = 'k', linestyle = '--')
+        plt.plot(c, -np.sqrt(c**2+1), color = 'k', linestyle = '--')
+        plt.scatter(0, 1, s = 5, zorder = 10, color = 'k')
+        plt.scatter(1, 0, s = 5, zorder = 10, color = 'k')
+        plt.scatter(-1, 0, s = 5, zorder = 10, color = 'k')
+        plt.scatter(0, -1, s = 5, zorder = 10, color = 'k')
+
+    plt.legend(loc = 'best')
+    plt.show()
+
+tester = [[0, 0], [3, 1]]
+
+tester_v = [0.5]
+minkowski_diagram(tester, tester_v, 8, False)
+        
+
+#LABELS AND AXIS CONNECTOR LINES!!
+
+
+        
+
+
 
 
