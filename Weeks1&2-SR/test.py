6ff9eaf0df12a816771ff76d03e292e3e057a589
Daniel Keerie <daniel@keerie.co.uk>
2025-06-21 23:26:32 +0100

    Added primed axis labels and started work on displaying transformed coordinates when given the frame to view them in. Code is incorrect however (all_coords is too full, should contain n*m entries where n is the number of events passed in and m is the number of velocities passed in- double check for loops!

---
+++ Weeks1&2-SR/test.py
@@ -0,0 +1,52 @@
+import numpy as np
+import sympy
+
+def function(x):
+    print(x)
+
+matrix = np.array([[1,2,3], [1,2,3]])
+
+vector = np.array([[1,2,3]])
+column = vector.transpose()
+
+print(matrix, vector)
+
+print(np.shape(vector))
+print(np.shape(column))
+
+
+print(np.matmul(vector, column))
+
+print(np.identity(3))
+
+sq = np.array([[1, 2, 0], [2, 1, 0], [0, 0, 2]])
+print(np.linalg.inv(sq))
+print(np.matmul(sq, np.linalg.inv(sq)))
+print(np.matmul(sq, np.identity(3), np.linalg.inv(sq)))
+
+
+print(sq @ np.linalg.inv(sq) @ sq)
+
+x = ('apple', 'banana')
+for index, fruit in enumerate(x):
+    print(index)
+
+
+x, y = sympy.symbols('x y')
+gamma, v = sympy.symbols('gamma v')
+lt = np.array([[gamma, -gamma*v], [-gamma*v, gamma]])
+
+tp, xp = lt @ np.array([x,y])
+
+print(tp, xp)
+
+empty = []
+a = [[0,1], [1,2]]
+for l in a:
+    empty.append(l)
+print(empty)
+
+empty_new = [empty]
+empty_new.append(empty)
+empty_new.append([[0,1]])
+print(empty_new)
